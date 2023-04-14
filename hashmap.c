cb6f3f4f48182d7b546edef47897cf8589e06a30
Replit user <>
2023-04-14 07:52:41 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -39,10 +39,6 @@ int is_equal(void* key1, void* key2){
 }
 
 
-void insertMap(HashMap * map, char * key, void * value) {
-
-
-}
 
 void insertMap(HashMap * map, char * key, void * value){
     if(map == NULL || key == NULL) return;
