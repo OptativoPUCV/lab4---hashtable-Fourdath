95451f727e55cc458fe0f28ef8921afc41598504
Replit user <>
2023-04-14 07:44:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -34,21 +34,8 @@ void enlarge(HashMap * map) {
 
 }
 
-
 HashMap * createMap(long capacity) { 
-    HashMap *map = (HashMap *) malloc(sizeof(HashMap));
-    if (map == NULL) {
-        return NULL;
-    } 
-    map->buckets = (Pair **) calloc(capacity, sizeof(Pair *));
-    if (map->buckets == NULL) {
-        free(map);
-        return NULL;
-    }
-    map->size = 0;
-    map->capacity = capacity;
-    map->current = -1;
-    
+
     return map;
 }
 
