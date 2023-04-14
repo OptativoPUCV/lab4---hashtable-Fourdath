c8089707ef514444c3f8ab490398e0b664ab2a68
Replit user <>
2023-04-14 07:45:10 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include "hashmap.h"
 
+
+typedef struct HashMap HashMap;
+int enlarge_called=0;
+
+struct HashMap {
+    Pair ** buckets;
+    long size; //cantidad de datos/pairs en la tabla
+    long capacity; //capacidad de la tabla
+    long current; //indice del ultimo dato accedido
+};
 
 Pair * createPair( char * key,  void * value) {
     Pair * new = (Pair *)malloc(sizeof(Pair));
